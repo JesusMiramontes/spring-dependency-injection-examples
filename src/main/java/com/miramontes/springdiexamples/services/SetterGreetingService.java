5d9374c3a74ea596b86930a71bbcabb0e938d12a
JesusMiramontes <4751856+JesusMiramontes@users.noreply.github.com>
2021-10-13 14:29:58 -0500

    Added SetterGreetingService

---
+++ src/main/java/com/miramontes/springdiexamples/services/SetterGreetingService.java
@@ -0,0 +1,11 @@
+package com.miramontes.springdiexamples.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SetterGreetingService implements GreetingService {
+    @Override
+    public String sayGreeting() {
+        return "Hello world. --Setter";
+    }
+}
